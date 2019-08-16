e56fe3b360b12b84101dfc2ba3ea6c797690d266
tong <paul.tong.cs@gmail.com>
2019-08-15 22:32:07 -0400

    add study

--- UmbrellaMate/App.js
+++ UmbrellaMate/App.js
@@ -43,7 +43,7 @@ const App = () => {
               <Text style={styles.sectionTitle}>Step One</Text>
               <Text style={styles.sectionDescription}>
                 Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your hahalalakakadadababa.
+                screen and then come back to see your changes.
               </Text>
             </View>
             <View style={styles.sectionContainer}>
