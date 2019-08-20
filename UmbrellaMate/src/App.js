ab7997daf88cc632ed46369ed8251dd7b3ec5ec2
tong <paul.tong.cs@gmail.com>
2019-08-19 23:23:44 -0400

    study basic knowledge

---
+++ UmbrellaMate/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import { Image, FlatList, StyleSheet, Text, View } from 'react-native';
+import {Study} from './components/study';
+
+function App() {
+  return (
+    <Study />
+  );
+}
+
+export default App;
---
+++ UmbrellaMate/src/styles/base.js
@@ -0,0 +1,33 @@
+/** 
+ *  basic style setting, sepecific setting should be in each component
+*/
+import {StyleSheet, Dimensions} from 'react-native'
+
+export const dimensions = {
+  fullHeight: Dimensions.get('window').height,
+  fullWidth: Dimensions.get('window').width
+}
+  
+export const colors  = {
+  primary: '#226B74',
+  secondary: '#254B5A',
+  tertiary: '#5DA6A7'
+}
+
+export const padding = {
+  sm: 10,
+  md: 20,
+  lg: 30,
+  xl: 40
+}
+
+export const fontSizes = {
+  sm: 12,
+  md: 18,
+  lg: 28
+}
+
+export const errorText = {
+  fontWeight: "700",
+  color: "red"
+}
